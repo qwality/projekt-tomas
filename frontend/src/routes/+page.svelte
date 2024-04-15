c26c88dfbfd7bebc388e2f7039a35ff055e92359
qwality <low.qwality@gmail.com>
2024-04-15 16:48:21 +0200

    prevod na skeleton

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Projekt - Tomas</h1>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Projekt Tomas</h1>
+	</div>
+</div>
